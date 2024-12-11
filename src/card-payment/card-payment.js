41dcd8c54906d4d38c5450e3454af70376ae2c32
Dean Smart <dean.smart@gene.co.uk>
2024-12-11 16:58:39 +0000

    updated card component to handle rendering fields

--- src/card-payment/card-payment.js
+++ src/card-payment/card-payment.js
@@ -37,9 +37,88 @@ function createFields() {
             },
             style: clientContext.style
         });
-        clientContext.renderFields(cardFields);
+        renderFields(cardFields);
     } else {
         clientContext.active(false);
+    }
+}
+
+function renderFields(cardFields) {
+    const clientNumberField = clientContext.cardFields.numberField,
+          clientCvvField = clientContext.cardFields.cvvField,
+          clientExpiryField = clientContext.cardFields.expiryField;
+
+    if (cardFields.isEligible()) {
+        let cardContainer = document.querySelector(clientNumberField.id),
+            cvvContainer = document.querySelector(clientCvvField.id),
+            cardExpiryContainer = document.querySelector(clientExpiryField.id);
+
+        const numberField = cardFields.NumberField({
+                placeholder: clientNumberField.placeholder,
+                inputEvents: {
+                    onBlur: (data) => {
+                        return clientNumberField.inputEvents.blur(data, cardContainer)
+                    },
+                    onFocus: () => {}
+                }
+            }),
+            cvvField = cardFields.CVVField({
+                placeholder: clientCvvField.placeholder,
+                inputEvents: {
+                    onBlur: (data) => {
+                        return clientCvvField.inputEvents.blur(data, cvvContainer)
+                    },
+                    onFocus: () => {}
+                }
+            }),
+            expiryField = cardFields.ExpiryField({
+                placeholder: clientExpiryField.placeholder,
+                inputEvents: {
+                    onBlur: (data) => {
+                        return clientExpiryField.inputEvents.blur(data, cardExpiryContainer)
+                    },
+                    onFocus: () => {}
+                }
+            });
+
+            if (cardContainer.innerHTML.trim() === "") {
+                numberField.render(clientNumberField.id);
+            }
+            if (cvvContainer.innerHTML.trim() === "") {
+                cvvField.render(clientCvvField.id);
+            }
+            if (cardExpiryContainer.innerHTML.trim() === "") {
+                expiryField.render(clientExpiryField.id);
+            }
+
+        document.querySelector(clientContext.submitButtonId).addEventListener('click', () => {
+            const isValid = clientContext.runAdditionalValidators()
+            if (isValid) {
+                cardFields.getState().then((data) => {
+                    if (data.isFormValid) {
+                        cardFields.submit().then(() => {
+                            //Submit success
+                        }).catch((error) => {
+                            console.error(error);
+                            clientContext.displayErrorMessage('Cannot validate payment.')
+                        });
+                    } else {
+                        let errorString = '';
+
+                        if (data.errors.includes('INVALID_NUMBER')) {
+                            errorString += 'Card number is not valid.';
+                        }
+                        if (data.errors.includes('INVALID_EXPIRY')) {
+                            errorString += ' ' + 'Expiry date is not valid.';
+                        }
+                        if (data.errors.includes('INVALID_CVV')) {
+                            errorString += ' ' + 'CVV is not valid.';
+                        }
+                        clientContext.displayErrorMessage(errorString)
+                    }
+                });
+            }
+        });
     }
 }
 
